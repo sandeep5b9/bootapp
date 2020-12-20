d5de1b3a625877e0da53e3e5967828287b346d42
wave2wf39 <wave2wf39@wave2wf297.iiht.tech>
2020-12-20 14:21:22 +0530

    bootapp

---
+++ src/main/java/com/wf/training/bootapp/BootappApplication.java
@@ -0,0 +1,13 @@
+package com.wf.training.bootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootappApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/wf/training/bootapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.wf.training.bootapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "index";
+	}
+}
